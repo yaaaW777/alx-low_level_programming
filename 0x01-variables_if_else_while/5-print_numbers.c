e5e79e64eec4a03c933d1513b8620e52d79eb29d
yaaaW777 <asanteantwi88@gmail.com>
2022-12-09 19:00:24 -0800

    advance task 1

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return Always 0 (success)
- */
-
-int main(void)
+#include <stdlib.h>                                                                                                                             
+#include <time.h>                                                                                                                               
+#include <stdio.h>                                                                                                                              
+/* more headers goes there */                                                                                                                   
+                                                                                                                                                
+/* betty style doc for function main goes there */                                                                                              
+/**                                                                                                                                             
+ *  * main - Entry point                                                                                                                           
+ *   *                                                                                                                                              
+ *    * Return: Always 0 (success)                                                                                                                   
+ *     */                                                                                                                                             
+int main(void)     
 {
 	int a;
 
-	for (a = 0; a <= 10; a++)
-		printf("%d", a);
-	putchar('\n');
-	return (0);
-}
+	for (a = 0; a
