23b10e6f1331af6d585e9ed12121b441568fc7b1
yaaaW777 <asanteantwi88@gmail.com>
2022-12-09 19:55:26 -0800

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,22 @@
-#include <stdlib.h>                                                                                                                             
-#include <time.h>                                                                                                                               
-#include <stdio.h>                                                                                                                              
-/* more headers goes there */                                                                                                                   
-                                                                                                                                                
-/* betty style doc for function main goes there */                                                                                              
-/**                                                                                                                                             
- *  * main - Entry point                                                                                                                           
- *   *                                                                                                                                              
- *    * Return: Always 0 (success)                                                                                                                   
- *     */                                                                                                                                             
-int main(void)     
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit number of base 10
+ * starting from 0, followed by new line.
+ * Return: 0
+ */
+int main(void)
 {
-	int a;
+	char c = '0';
 
-	for (a = 0; a
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 
