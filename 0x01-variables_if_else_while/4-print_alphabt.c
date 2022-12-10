07876d283dbe5088025fe2cfc4ec63c3cbc2c830
yaaaW777 <asanteantwi88@gmail.com>
2022-12-09 17:57:51 -0800

    print alphabet 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  *
  * main Entry point
@@ -7,17 +9,18 @@
  */
 
 int main(void)
-{
-	char c;
 
-	c = 'a';
-	while
-		(c <= 'z') {
-			if ((c != 'q'&& c != 'e') && c <= 'z')
-				putchar(c);
-			c++;
+{
+char c;
+
+c = 'a';
+while
+( c <= 'z') {
+if (( c != 'q' && c != 'e') && c <= 'z')
+putchar(c);
+c++;
 		}
-	putchar('\n');
-	return(0);
+putchar('\n');
+return(0);
 }
 				
