0cb1ffd3d4188f984da92c7c3a056c58792fab50
yaaaW777 <asanteantwi88@gmail.com>
2022-12-12 13:06:55 -0800

    last digit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,26 +1,8 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description: print the alphabet in lower case
- * and the in uppercase, follow by a new line
- * Return: O
- */
-int main(void)
-{
-char c = 'a';
-while (c <= 'z')
-{
-putchar(c);
- c++;
-}
-
-c = 'A';		
-while (c <= 'Z')
-{
-putchar(c);
-c++;
-}
-
-putchar('\n');
-return (0);
-}
+ * main Entry point
+ *
+ * Return Always o (success)
+ * /
+ 
+ int main(void)
