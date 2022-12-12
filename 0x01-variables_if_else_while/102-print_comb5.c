49a924885392c10813cf260e3f25c1d1a82f2b6c
yaaaW777 <asanteantwi88@gmail.com>
2022-12-12 06:23:59 -0800

    advance task 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,11 +10,38 @@ int main(void)
 {
 	int c = 0;
 	int f_d;
-	int 1_d;
+	int l_d;
 
 	int c2;
 	int f_d2;
-	int 1_d2;
+	int l_d2;
+
 	while (c <= 98)_
 	{
-		f_d = (c / 10 = '0');
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+		c2 = 0;
+		(c2 <= 99)
+		{
+			f_d2 = (c2 / 10 + '0');
+			l_d2 = (c2 % 10 + '0');
+
+			if (c < c2)
+			{
+				putchar(f_d);
+				putchar(l_d);
+				putchar(' ');
+				putchar(f_d2);
+			}
+		}
+		c2++;
+	}
+	c++;
+
+}
+putchar('\n');
+return (0);
+
+}
+
+		
