ad7e656d6a6cae2f1cc836987fd0e8235e893880
yaaaW777 <asanteantwi88@gmail.com>
2022-12-13 19:51:40 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *
+ * @n : number 0 or 1
+ */
+int printf_sign(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test = -1;
+		_putchar('-');
+	}
+	return (test);
+}
