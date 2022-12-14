5a27717901ac0a7d0600226a759f4bddcb38b1ae
yaaaW777 <asanteantwi88@gmail.com>
2022-12-14 13:52:59 -0800

    task 1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
-	if (a > b)
-	{
-		if (b > c)
-			largest = a;
-		else if (a > c)
-			largest = a;
-		else 
-			largest = c;
+if (a > b)
+{
+if (b > c)
+largest = a;
+else if (a > c)
+largest = a;
+else 
+largest = c;
 
-	}
-	else
-	{
-		if (b < c)
-			largest = c;
-		else
-			largest = b;
-	}
-	return (largest);
+}
+else
+{
+if (b < c)
+largest = c;
+else
+largest = b;
+}
+return(largest);
 }
