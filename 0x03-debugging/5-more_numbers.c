de8f881021580eb8f2b0b8137441c21434ef00e5
yaaaW777 <asanteantwi88@gmail.com>
2022-12-16 03:23:26 -0800

    task 5

--- 0x03-debugging/5-more_numbers.c
+++ 0x03-debugging/5-more_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * more_numbers - number fct
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  *
  * Return: no return
  */
@@ -13,9 +14,9 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-				_putchar('1');
-			_putchar(j % 10 + '0');
+				putchar('1');
+			putchar(j % 10 + '0');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
