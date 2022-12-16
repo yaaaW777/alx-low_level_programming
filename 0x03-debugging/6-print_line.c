03636ca27aceda03bfa962e6649ad9e247dcb080
yaaaW777 <asanteantwi88@gmail.com>
2022-12-16 03:36:33 -0800

    task 6

---
+++ 0x03-debugging/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - printing lines
+ * @n: number oftimes
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
