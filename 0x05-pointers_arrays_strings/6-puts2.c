70ab725dbff75c0baafb97e93cbc11f916b8e719
yaaaW777 <asanteantwi88@gmail.com>
2022-12-19 09:11:09 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * puts2 - prints one character out of two
  * @str: input
@@ -6,13 +7,14 @@
  */
 void puts2(char *str)
 {
-int j = 0;
-for (; str[i] != '\0'; i++)
-{
-if ((i % 2) == 0)
-_putchar(str[i]);
-else
-continue;
-}
-_piutchar('\n');
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
 }
