14cb53fc246559867ba8e1bc59d422e89f804fc0
yaaaW777 <asanteantwi88@gmail.com>
2022-12-19 06:25:23 -0800

    task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - update the value of n to 98
  * @n; input
- * Return: n
+ * Return: n success
  */
 void reset_to_98(int *n)
 {
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,15 @@
 #define MAIN_H
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+
+
+
+
+
+
+
+
 int _putchar(char c);
 
 
