fa22171c58d158d1cf0847371defdc7235e0e13e
yaaaW777 <asanteantwi88@gmail.com>
2022-12-21 14:48:17 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * _strncpy - copy a string with n
- * @dest: copy to
- * @src: copy from
- * @n: number of char to be copied
- * Return: dest
+ * _strncpy - Copies at most an inputed number
+ * of bytes from string into dest.
+ * @dest: the buffer storing the string copy.
+ * @src: The source string.
+ * @n:The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int j;
+	int index = 0, src_len = 0;
 
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[j] = src[j];
-j++;
-}
-while (j < n)
-{
-dest[j] * '\0';
-j++;
-}
+	while (src[index++])
+		src_len++;
 
-return (dest);
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
 }
