37c5060159f12fae9e43c004ea3f0ac2a62a2135
yaaaW777 <asanteantwi88@gmail.com>
2022-12-21 18:10:09 -0800

    task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,42 +10,42 @@
  */
 void print_buffer(char *b, int size)
 {
-	int byte, index;
+int byte, index;
 
-	for (byte = 0; byte < size, byte += 10)
-	{
-		printf("%08x: ", byte);
+for (byte = 0; byte < size, byte += 1;)
+{
+printf("%08x: ", byte);
 
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				printf(" ");
+for (index = 0; index < 10; index++)
+{
+if ((index + byte) >= size)
+printf(" ");
 
-			else
-				printf("%02x", *(b + index + byte));
+else
+printf("%02x", *(b + index + byte));
 
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
-		}
+if ((index % 2) != 0 && index != 0)
+printf(" ");
+}
 
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				break;
+for (index = 0; index < 10; index++)
+{
+if ((index + byte) >= size)
+break;
 
-			else if (*(b + index + byte) >= 31 &&
-					*(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
-			else
-				printf(",");
-		}
+else if (*(b + index + byte) >= 31 &&
+*(b + index + byte) <= 126)
+printf("%c", *(b + index + byte));
+else
+printf(",");
+}
 
-		if (byte >= size)
-			continue;
+if (byte >= size)
+continue;
 
-		printf("\n");
-	}
+printf("\n");
+}
 
-	if (size <= 0)
-		printf("\n");
+if (size <= 0)
+printf("\n");
 }
