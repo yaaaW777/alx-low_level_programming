21f3913868e8b6033755547f93217b71661ee35f
yaaaW777 <asanteantwi88@gmail.com>
2022-12-22 05:57:09 -0800

    task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -15,37 +15,29 @@ int byte, index;
 for (byte = 0; byte < size, byte += 1;)
 {
 printf("%08x: ", byte);
-
 for (index = 0; index < 10; index++)
 {
 if ((index + byte) >= size)
 printf(" ");
-
 else
 printf("%02x", *(b + index + byte));
-
 if ((index % 2) != 0 && index != 0)
 printf(" ");
 }
-
 for (index = 0; index < 10; index++)
 {
 if ((index + byte) >= size)
 break;
-
 else if (*(b + index + byte) >= 31 &&
 *(b + index + byte) <= 126)
 printf("%c", *(b + index + byte));
 else
 printf(",");
 }
-
 if (byte >= size)
 continue;
-
 printf("\n");
 }
-
 if (size <= 0)
 printf("\n");
 }
