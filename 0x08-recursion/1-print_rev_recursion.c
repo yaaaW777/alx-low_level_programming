eb53305a1d01638fdc49cc00a3ac7f00f6c5fe8e
yaaaW777 <asanteantwi88@gmail.com>
2023-01-05 09:29:11 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,11 +6,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_putchar(*s);
-		_print_rev_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
