1dcc9e48b2cedd7ca35e503c1adc0aca9eed08eb
yaaaW777 <asanteantwi88@gmail.com>
2023-01-16 06:34:55 -0800

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * int_dog - initializes a variable of type struct dog.
+ * @d: pointer to structure.
+ * @name: first member.
+ * @age: second member.
+ * @owner: third member.
+ *
+ * Return: void.
+ */
+void int_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == 0)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
