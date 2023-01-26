6efe8c2e529efb6467db99689d857407b8e94143
yaaaW777 <asanteantwi@gmail.com>
2023-01-26 05:01:06 -0800

    hello holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* bmain - function exeucted before main
+* Return: no return.
+*/
+
+void _attribute_ ((constructor)) bmain()
+{
+printf("You're beat! and yet, you must alllow");
+Printf(",/nI bore my house upon my back!/n";
+}
