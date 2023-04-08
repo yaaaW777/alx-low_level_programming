27700a895ac1466ceedffd739eeeaf5e6e038c8f
yaaaW777 <asanteantwi@gmail.com>
2023-04-07 17:52:18 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -52,7 +52,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
